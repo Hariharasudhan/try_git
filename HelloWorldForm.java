a9b4344814ccf738586f470e5a8e181b8cc0dd10
Hariharasudhan <hariharannitt@gmail.com>
2013-07-18 17:45:28 +0530

    HelloWorldForm.java
    Struts2 "Hello World" program ..
    This is the Form Class

---
+++ HelloWorldForm.java
@@ -0,0 +1,25 @@
+/* A simple Struts2 "Hello World" Application */
+/* Put this JavaFile under the form package */
+
+
+package form;
+
+import org.apache.struts.action.ActionForm;
+
+public class HelloWorldForm extends ActionForm 
+{
+ 
+  /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String message;
+ 
+	public String getMessage() {
+		return message;
+	}
+ 
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
